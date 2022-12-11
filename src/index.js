a04145dc3cb3a19c78976b3ff79141e6d4656c68
kirpichev228 <kirpichev228@mail.ru>
2022-12-11 15:21:10 +0300

    m

--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ import './styles/index.css';
 
 const mult = (a, b) => a * b;
 
-console.log(mult(2, 4));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const mode = process.env.NODE_ENV || 'development';
 const devMode = mode === 'development';
 const target = devMode ? 'web' : 'browserslist';
